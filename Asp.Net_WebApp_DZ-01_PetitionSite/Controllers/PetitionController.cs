72acb92d4f8a77b6b9e0e349b3ce71f4538b77c8
Makc <mks59k@gmail.com>
2019-03-03 00:14:55 +0200

    Добавление контроллера петиций, и представления.

---
+++ Asp.Net_WebApp_DZ-01_PetitionSite/Controllers/PetitionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.Net_WebApp_DZ_01_PetitionSite.Controllers
+{
+    public class PetitionController : Controller
+    {
+        // GET: Petition
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Petition
+        public ActionResult All()
+        {
+            return View();
+        }
+    }
+}
