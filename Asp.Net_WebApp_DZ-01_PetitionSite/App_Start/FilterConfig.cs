929b4d96b2672283d8845d46dbba837f4d365d67
Makc <mks59k@gmail.com>
2019-03-02 20:32:44 +0200

    Add project files.

---
+++ Asp.Net_WebApp_DZ-01_PetitionSite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.Net_WebApp_DZ_01_PetitionSite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Asp.Net_WebApp_DZ-01_PetitionSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.Net_WebApp_DZ_01_PetitionSite.MvcApplication" Language="C#" %>
