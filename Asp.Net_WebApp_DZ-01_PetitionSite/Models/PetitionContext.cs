b92263fc4229698b684718d2043ad2b09cfe54cf
Makc <mks59k@gmail.com>
2019-03-03 00:17:16 +0200

    Добавлен пакет Entity.

---
+++ Asp.Net_WebApp_DZ-01_PetitionSite/Models/PetitionContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net_WebApp_DZ_01_PetitionSite.Models
+{
+    public class PetitionContext /*: DbContext*/
+    {
+    }
+}
